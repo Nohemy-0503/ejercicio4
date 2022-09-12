c0d313f943c59e0b566817312c0b929b0f95e399
Marcela Villatoro <SMSS072922@ugb.edu.sv>
2022-09-11 21:52:56 -0600

    Agregar archivos de proyecto.

---
+++ ejercicio4/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double numero1 = Convert.ToDouble(textBox1.Text);
+            double numero2 = Convert.ToDouble(textBox2.Text);
+            if (checkBox1.Checked == true)
+            {
+                double suma = numero1 + numero2;
+                label3.Text = "Resultado:" + suma.ToString();
+            }
+            if (checkBox2.Checked == true)
+            {
+                double resta = numero1 - numero2;
+                label3.Text  = "Resultado:" + resta.ToString();
+            }
+        }
+    }
+}
